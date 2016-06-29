c70552c3d857eb1efb01145c13eae11115b139fa
Salviano Ludgério <salviano.lfg@gmail.com>
2016-06-29 09:46:50 -0300

    Add utils commands
    - print clone url
    - list repositories names

--- awscli_alias.sh
+++ awscli_alias.sh
@@ -6,3 +6,9 @@ alias create-repository='aws codecommit create-repository --repository-name'
 alias list-repositories='aws codecommit list-repositories --sort-by lastModifiedDate --order descending'
 alias get-repository='aws codecommit get-repository --repository-name'
 alias delete-repository='aws codecommit delete-repository --repository-name'
+
+#Print clone url
+aws codecommit get-repository --repository-name <name> --query repositoryMetadata.cloneUrlSsh | tr -d "\""
+
+#List repositories names
+aws codecommit list-repositories --sort-by lastModifiedDate --order descending --query repositories[].repositoryName[]
